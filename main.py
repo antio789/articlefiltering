72c68cf3648fadba42d3de3c29091bbf05282fd2
bob-the-second <mail@mail.com>
2026-04-28 12:17:16 +0200

    updated prompting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pypdf import PdfReader
 import json
 import glob
 import tiktoken
+import fitz
 
 '''READ CONTENT DEFINITIONS'''
 def json_read(path):
@@ -20,8 +21,22 @@ def pdf_read(path):
     reader = PdfReader(path)
     return "\n".join(p.extract_text() for p in reader.pages)
 
+def pdf_img(path):
+    doc = fitz.open(path)
+    images= []
+
+    for page in doc:
+        pix = page.getPixmap(matrix=fitz.Matrix(2,2))
+        imgB = pix.tobytes("png")
+        images.append(imgB)
+    return images
+
+#models tested mistral, ministral, mistrallite, nemotron, qwen, gemma3n:e2b.
+# ministral-3:8b: very fast and writes good reasoning, not very accurate, pretty dumb, forgets text.
+# nemotron is very fast but not very smart
+# gemma 3 very fast but unstable
 def llm_prompt(string):
-  response: ChatResponse = chat(model='mistral', messages=[
+  response: ChatResponse = chat(model='ministral-3:8b', messages=[
     {
       'role': 'user',
       'content': string,
@@ -40,15 +55,22 @@ article_prompt = rf("article_prompt")
 review_prompt = rf("review_prompt")
 
 general_questions = json_read('q_general.json')
+filters = json_read('q_filtering.json')
 
 articles_list = glob.glob("articles/*.pdf")
 
 article_text = pdf_read(articles_list[0])
+print('article read')
+
+'PDF ARRANGING'
 
 
 'FILTERING'
-prompt = general_prompt + "\nquestion:\n" + general_questions[0].get("question") + "\nArticle to read:\n" + article_text
-#print(prompt)
-#llm_prompt(prompt)
-#print(count_tokens(prompt))
+for i in general_questions:
+    prompt = "Article to read:\n" + article_text + "\nquestion:\n" + i.get("question") + ' \n'+ general_prompt
+    #llm_prompt(prompt)
 
+prompt_questions = "Article to read:\n" + article_text + "\nquestion:\n" + filters[0].get("question") + ' \n'+ review_prompt
+#print(count_tokens(prompt))
+#print(prompt_questions)
+#llm_prompt(prompt_questions)

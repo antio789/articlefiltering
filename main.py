f270184a672822bfb8cdc049c573c3615e94f4dd
bob-the-second <mail@mail.com>
2026-05-06 12:34:29 +0200

    output prompt clarified for more reliable responses export to json file automatised article reading

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from ollama import chat, generate
-from ollama import ChatResponse
 from pypdf import PdfReader
 import json
 import glob
 import tiktoken
-import fitz
 import logging
 
 logger = logging.getLogger(__name__)
@@ -29,10 +27,10 @@ def read_pdf(path):
 # gemma 3, fastest can go up to 128k, 86k to leave space to increase output size  - gemmatest86k
 # qwen3.5, reasoning model, but at a slower speed and smaller context requires deeper analysis qwen
 def llm_prompt(string):
-    response = generate(model='qwen48k', prompt=string, options={'temperature':0,'num_ctx':42000,'num_predict':12000})
-    time = int(response['total_duration'])/1000000
+    response = generate(model='gemmatest86k', prompt=string, options={'temperature':0,'num_predict':12000,'seed':15})
+    time = int(int(response['total_duration'])/1000000000)
     logger.info(time)
-    print(time + 'time to run in seconds')
+    print(str(time) + ' seconds of runtime')
     return response['response']
 
 
@@ -53,11 +51,12 @@ article_filters = read_json('q_articles.json')
 
 articles_list = glob.glob("articles/*.pdf")
 
-article_text = read_pdf(articles_list[0])
+#article_text = read_pdf(articles_list[0])
 
 
 'LLM FILTERING'
 def classify_article(text):
+    print(text)
     filter_list = []
     #askes the main questions: 1.if its anaerobic digestion otherwise return, 2.if it's a review
     for i in general_questions:
@@ -66,18 +65,30 @@ def classify_article(text):
         response.lower()
         print(response)
         reasoning, answer = response.split("###")
-        filter_list.append(answer)
+        filter_list.append({
+            "qid": i.get("qid"),
+            "question": i.get("question"),
+            "reasoning": reasoning.strip(),
+            "answer": answer.strip()
+        })
         if i == 0 and "false" in answer:
             return filter_list
     #splits the questions in between reviews and articles as the prompt is slightly different, true means review
-    if "true" in filter_list[1]:
+    if "true" in filter_list[1]["answer"].lower():
         print("review")
         for q in review_filters:
             print(q.get("qid"))
             prompt_question = "Article to read:\n" + text + "\nquestion:\n" + q.get("question") + ' \n' + review_prompt
             output = llm_prompt(prompt_question)
             print(output)
-            filter_list.append(output)
+            reasoning, answer = response.split("###")
+            logger.info(str(q.get("qid")) + str(reasoning))
+            filter_list.append({
+                "qid": q.get("qid"),
+                "question": q.get("question"),
+                "reasoning": reasoning.strip(),
+                "answer": answer.strip()
+            })
     else:
         print("article")
         for q in article_filters:
@@ -85,8 +96,16 @@ def classify_article(text):
             prompt_question = "Article to read:\n" + text + "\nquestion:\n" + q.get("question") + ' \n' + article_prompt
             output = llm_prompt(prompt_question)
             print(output)
-            filter_list.append(output)
+            reasoning, answer = response.split("###")
+            logger.info(str(q.get("qid"))+ str(reasoning))
+            filter_list.append({
+                "qid": q.get("qid"),
+                "question": q.get("question"),
+                "reasoning": reasoning.strip(),
+                "answer": answer.strip()
+            })
     return filter_list
+
 
 'num_predict allows for changing output context length, need specific model'
 def clean_article(text):
@@ -99,22 +118,44 @@ def clean_article(text):
     Please do not change the text beyond the formatting process.
     Do not write any commentary
     '''
-    prompt2 = "Of this scientific article extract only the methodology section. return ONLY the extracted text, with no commentary, explanations, or formatting"
+    prompt2 = "of this scientific article please return ONLY the line number of where the reference start, much appreciated"
     #print(text+ "\n"+prompt2)
-    text_prompt = text+ "\n"+prompt
+    text_prompt = text+ "\n"+prompt2
 
 #qwen48k(maximum size) - gemma3.128k
-    response = generate(model='qwen48k', prompt=text_prompt, options={'num_predict':48000,'num_ctx':48000})
+    response = generate(model='qwen48k', prompt=text_prompt, options={'temperature':0,'num_predict':48000},)
+    logger.info(response['think'])
     return response['response']
 
     #print(count_tokens(prompt))
     #print(prompt_questions)
     #llm_prompt(prompt_questions)
 
+def run_filtering():
+    logger.info('start')
+    idart=0
+    for art in articles_list:
+        article_text = read_pdf(art)
+        filtered = classify_article(article_text)
+        name= 'article'+str(idart)+'.json'
+        with open(name, 'w') as f:
+            json.dump(filtered, f)
+        idart+=1
+    logger.info('end')
+
+def run_oneQ(id, text):
+    qid=id-1
+    if qid==43:
+        print("note id 43 is not present")
+        return
+    if id>len(review_filters)+1:
+        print("outside scope")
+        return
+    if qid>43: qid=qid-1
+    print(review_filters[qid].get("qid"))
+    prompt_question = "Article to read:\n" + text + "\nquestion:\n" + review_filters[qid].get("question") + ' \n' + review_prompt
+    output = llm_prompt(prompt_question)
+    print(output)
+
 logging.basicConfig(filename='llmlog.log',format='%(asctime)s %(message)s',level=logging.INFO)
-logger.info('start')
-filtered = classify_article(article_text)
-print(filtered)
-with open('filtered_articles', 'w') as outfile:
-    outfile.write(str(filtered))
-logger.info('end')
+print(articles_list[0])

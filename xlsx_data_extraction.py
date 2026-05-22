ae41174a11604fc1fd515378618dfab317d2f3ce
antio789 <antio789@hotmail.fr>
2026-05-22 14:40:09 +0200

    -

--- xlsx_data_extraction.py
+++ xlsx_data_extraction.py
@@ -1,50 +1,18 @@
 import csv
+import glob
 import json
+import os
+
 
 ### Gathering the filters obtained from the manual filtering for the article list
 
-def extract_dois(csv_file):
-    DOI = []
-    with open(csv_file, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f,delimiter=";")
-        for row in reader:
-            doi = row[2]
-            id = row[1]
-            DOI.append([id,doi])
-    return DOI
-
-
-def get_article_id(doi):
-    links = extract_dois("data/articlelist.csv")
-    for i in range(1,len(links)):
-        if str(doi) in str(links[i][1]):
-            return links[i][0]
-    return 0
-
-def get_manual_review(csv_file, article_id):
-    results = []
-    with open(csv_file, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter=",")
-
-        for row in reader:
-            doi = str(row[5])
-            id = get_article_id(doi)
-            if id != 0 and str(id) == str(article_id):
-                datapoint = [id, row[7]]
-                if datapoint not in results:
-                    results.append(datapoint)
-    #print(results)
-    return results
-
-### Gather results from the LLM filtering on the article list
-
-
+#should ignore what is not in this list like freezing
 pretreatment_map = {
-    1: "mechanical desintegration",
-    2: "thermal",
+    1: "mechanical disintegration",
+    2: "thermal",#might need clear definition of what thermal means, like termperature of water bath shouldnt be included.
     3: "ultrasonic",
-    4: "pressure",
-    5: "pulsed electric field",
+    4: "pressure",#might need some better definition, as includes thermal or ultrasonic methods as well as pressure to seperate liquid from solid
+    5: "electric",#modify the question to inluce electrolysis more directly
     6: "alkaline",
     7: "acidic",
     8: "oxidizer",
@@ -52,7 +20,7 @@ pretreatment_map = {
     10: "other",
     11: "enzyme",
     12: "fungi",
-    13: "microbial consortium",
+    13: "microbial consortium",#figure out what to do with ensilage proably exclude it
     14: "biological active substrate",
     15: "nanoparticles",
     16: "disinhibition",
@@ -61,27 +29,55 @@ pretreatment_map = {
     58: "microaerobic"
 }
 
-
-def get_llm_review(json_file):
+def get_manual_review_article(artid): #Make sure there are no duplicates in the filtering list
+    doi = ""
+    with open("data/articlelist.csv", 'r', encoding='utf-8') as f:
+        reader = csv.reader(f,delimiter=";")
+        for row in reader:
+            if artid == str(row[1]):
+                doi=row[2]
+                break
+        if doi == "":
+            raise Exception(f"Error article not found for id: {artid}")
     results = []
-    article_id = json_file.replace(".json","").replace("ptoutput/","")
-    with open(json_file, 'r', encoding='utf-8') as file:
+    with open('data/pretreatment_complete_list.csv',encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=";")
+        for row in reader:
+            if doi == row[5]:
+                pretreatment = row[7].strip().lower()
+                datapoint = [artid, pretreatment]
+                if datapoint not in results and pretreatment in pretreatment_map.values():
+                    results.append(datapoint)
+    return results
+
+
+
+### Gather results from the LLM filtering on the article list
+
+def get_llm_review(jfile,art_id):
+    results = []
+    with open(jfile, 'r', encoding='utf-8') as file:
         data = json.load(file)
         filters = data.get("filters")
         for item in filters:
             answer = str(item.get("answer")).lower()
-            if answer == "true":
+            if "true" in answer:
                 qid = item.get("qid")
                 pretreatment = pretreatment_map[qid]
-                datapoint = [article_id, pretreatment]
+                datapoint = [art_id, pretreatment]
                 results.append(datapoint)
     return results
 
 ### Compare results from both sources
 
-def compare(manual, llm):
-    manual_filtered = [item for item in manual if item[1] != pretreatment_map[10]]
-    llm_filtered = [item for item in llm if item[1] != pretreatment_map[10]]
+def compare(manual, llm, include_other=False):
+    if include_other:
+        manual_filtered = manual
+        llm_filtered = llm
+    else:
+        manual_filtered = [item for item in manual if item[1] != pretreatment_map[10]]
+        llm_filtered = [item for item in llm if item[1] != pretreatment_map[10]]
+
     correct_predictions = 0
     false_predictions = 0
     missed_predictions = 0
@@ -108,17 +104,12 @@ def compare(manual, llm):
     f1_score = 0
 
     if (correct_predictions + false_predictions) > 0:
-        precision = (
-                correct_predictions / (correct_predictions + false_predictions)
-        )
+        precision = ( correct_predictions / (correct_predictions + false_predictions))
     if (correct_predictions + missed_predictions) > 0:
-        recall = (
-                correct_predictions / (correct_predictions + missed_predictions)
-        )
+        recall = (correct_predictions / (correct_predictions + missed_predictions))
     if (precision + recall) > 0:
-        f1_score = (
-                2 * precision * recall / (precision + recall)
-        )
+        f1_score = (2 * precision * recall / (precision + recall))
+
     report = {
         "manual_review": manual_filtered,
         "llm_review": llm_filtered,
@@ -132,24 +123,119 @@ def compare(manual, llm):
         "false_items": false_items,
         "missed_items": missed_items
     }
-
     return report
+
+def compare_summary(manual, llm, include_other=False):
+    if include_other:
+        manual_filtered = manual
+        llm_filtered = llm
+    else:
+        manual_filtered = [item for item in manual if item[1] != pretreatment_map[10]]
+        llm_filtered = [item for item in llm if item[1] != pretreatment_map[10]]
+
+    correct_predictions = 0
+    false_predictions = 0
+    missed_predictions = 0
+
+    for item in llm_filtered:
+        if item in manual_filtered:
+            correct_predictions += 1
+        else:
+            false_predictions += 1
+
+    for item in manual_filtered:
+        if item not in llm_filtered:
+            missed_predictions += 1
+
+    precision = 0
+    recall = 0
+    f1_score = 0
+
+    if (correct_predictions + false_predictions) > 0:
+        precision = (correct_predictions / (correct_predictions + false_predictions))
+    if (correct_predictions + missed_predictions) > 0:
+        recall = (correct_predictions / (correct_predictions + missed_predictions))
+    if (precision + recall) > 0:
+        f1_score = (2 * precision * recall / (precision + recall))
+
+    return {
+        "correct_predictions": correct_predictions,
+        "false_predictions": false_predictions,
+        "missed_predictions": missed_predictions,
+        "precision": round(precision, 4),
+        "recall": round(recall, 4),
+        "f1_score": round(f1_score, 4)
+    }
+
+def compare_question(question_name, all_manual, all_llm):
+
+    manual_filtered = [
+        item for item in all_manual
+        if item[1] == question_name
+    ]
+
+    llm_filtered = [
+        item for item in all_llm
+        if item[1] == question_name
+    ]
+
+    return compare(
+        manual_filtered,
+        llm_filtered,
+        include_other=True
+    )
+
+def run_all():
+    ensure_results_dirs()
+
+    json_files = glob.glob("ptoutput/*.json")
+
+    all_manual = []
+    all_llm = []
+
+    for article_output in json_files:
+        processing_id = (os.path.basename(article_output).replace(".json", ""))
+        print(processing_id + " article id check")
+
+        manual_review = get_manual_review_article(processing_id)
+        llm_review = get_llm_review(article_output,processing_id)
+
+        article_report = compare(manual_review, llm_review, include_other=False)
+        save_report(f"results/articles/{processing_id}.json", article_report)
+
+        all_manual.extend(manual_review)
+        all_llm.extend(llm_review)
+
+    global_report = compare_summary(all_manual, all_llm, include_other=False)
+    save_report("results/all/global_report_2205_1430.json", global_report)
+
+    for qid, question_name in pretreatment_map.items():
+        question_report = compare_question(question_name, all_manual, all_llm)
+
+        filename = f"results/questions/questions_{qid}.json"
+        save_report(filename, question_report)
+
+    print("Finished")
+
+
+### file handling
 
 def save_report(output_file, report):
     with open(output_file, "w", encoding="utf-8") as file:
         json.dump(report, file, indent=2)
 
 
-json_file = "ptoutput/1117.json"
-article_id = json_file.replace(".json", "").replace("ptoutput/", "")
-
-manual_review = get_manual_review( "data/pretreatment_data.csv", article_id )
-llm_review = get_llm_review(json_file)
-
-report = compare(manual_review,llm_review)
-save_report("testfile",report)
+def ensure_results_dirs():
+    os.makedirs("results/all", exist_ok=True)
+    os.makedirs("results/articles", exist_ok=True)
+    os.makedirs("results/questions", exist_ok=True)
 
 
+
+### Run
+
+#87 should be checked as missing some pretreatments
+run_all()
 
 
 ### Helper functions not in use
@@ -166,15 +252,3 @@ def list_filters():
         return pretreatment_filters
 
 
-def get_manual_list(csv_file):
-    results = []
-    with open(csv_file, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f,delimiter=";")
-        for row in reader:
-            doi = str(row[5])
-            id = get_article_id(doi)
-            if id != 0:
-                datapoint = [id,row[7]]
-                if datapoint not in results:
-                    results.append(datapoint)
-    return results
